6008454a38da2f5992b96e0f021945412850014f
Diede <diedevandijk8@gmail.com>
2022-03-10 19:40:03 +0100

    .

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import {update as updateSnake, draw as drawSnake,  SNAKE_SPEED} from './snake.js'
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
 
 let lastRenderTime = 0
 const gameBoard = document.getElementById('game-board')
